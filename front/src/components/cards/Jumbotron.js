ec8cf6e682085b139b60b9ab0d043d00d36e74d2
juliocesaralvesmoreira <juliocesaralvesmoreirajcam@gmail.com>
2023-01-01 14:05:00 -0300

    Jumbroton

--- front/src/components/cards/Jumbotron.js
+++ front/src/components/cards/Jumbotron.js
@@ -1,3 +1,12 @@
 export default function Jumbotron(){
-
+    return (
+<div className="container-fluid bg-primary">
+    <div className="row">
+        <div className="col text-center p-5 bg-light">
+            <h1>Título a Página</h1>
+            <p className="lead">Subtítulo da página</p>
+        </div>
+    </div>
+</div>
+    );
 }
